0574fa6110c7fbd0f78e2aff033821a2203f31bd
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-08 19:40:18 -0400

    mom mode

--- src/views/MomMode.js
+++ src/views/MomMode.js
@@ -30,12 +30,14 @@ export default function MomMode() {
                 <div className="account-row">
                     <figure className="account-row__image-slot">
                         <FontAwesomeIcon className="account-row__logo" icon={["fab", "spotify"]} />
-                        <h2 className="account-row__heading">Premium Individual</h2>
-                        <h2 className="account-row__text">Monthly subscription</h2>
                     </figure>
+                    <h2 className="account-row__heading">Premium Individual</h2>
+                    <h2 className="account-row__text">Monthly subscription</h2>
+                    <FontAwesomeIcon className="account-row__arrow" icon={"chevron-right"} />
                 </div>
+                <p className="cancel">To close (delete) your account permanently, contact customer support.</p>
                 <h3 className="mom-mode__heading">Mom Mode</h3>
-                <p className="mom-mode__description">Enable Mom Mode to keep track of your favorite songs while you're expecting and we'll present you with a "Delivery Wrapped" playlist after the big day.</p>
+                <p className="mom-mode__text">Enable Mom Mode to keep track of your favorite songs while you're expecting and we'll present you with a "Delivery Wrapped" playlist after the big day.</p>
                 <label className="mom-mode__switch">
                     <input className="mom-mode__checkbox" type="checkbox" onClick={() => {setMomMode(!momMode)}}/>
                     <span className="mom-mode__slider"></span>
@@ -44,7 +46,6 @@ export default function MomMode() {
                     <h4 className="date-row__heading">Due Date</h4>
                     <input className="date-row__input" type="date" />
                 </label>
-                <p className="cancel">To close (delete) your account permanently, contact customer support.</p>
                 <div className="nav-row">
                     <Link to="/" className="nav-row__item">
                         <FontAwesomeIcon className="nav-row__icon" icon={"house"} />
