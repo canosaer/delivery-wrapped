93e3b0b6256ba79940c62ed8200c7cdd605d2fe2
canosaer <67963828+canosaer@users.noreply.github.com>
2022-05-08 11:26:34 -0400

    home screen

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,39 +2,41 @@ import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import {Context} from '../store/store'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import accent from '../img/accent.png'
+import mom from '../img/mom.jpg'
+import baby from '../img/baby.jpg'
+import celebration from '../img/celebration.png'
 
 export default function Home() {
     const [state, dispatch] = useContext(Context)
 
     return(
         <main className="home">
+            <img className="home__accent" src={accent}></img>
             <section className="home__container">
-                <FontAwesomeIcon className="home__logo" icon={["fab", "spotify"]} />
+                <a className="home__link" href="https://www.spotify.com/us/" target="_blank">
+                    <FontAwesomeIcon className="home__logo" icon={["fab", "spotify"]} />
+                </a>
                 <h1 className="home__heading">Delivery Wrapped</h1>
-                <h1 className="home__subheading">App Views</h1>
                 <div className="home__row-display">
                     <Link to="" className="row">
                         <p className="row__rank">#1</p>
                             <div className="row__image-slot">
-                                <FontAwesomeIcon className="row__image" icon="person-breastfeeding" />
+                                <img className="row__image" src={mom} alt="expecting mother" />
                             </div>
-                        {/* <figure className="row__image"></figure> */}
                         <p className="row__title">Mom Mode</p>
                     </Link>
                     <Link to="" className="row">
                         <p className="row__rank">#2</p>
-                        {/* <figure className="row__image"></figure> */}
                         <div className="row__image-slot">
-                            <FontAwesomeIcon className="row__image" icon="baby" />
+                            <img className="row__image" src={baby} alt="baby" />
                         </div>
                         <p className="row__title">Wrapped Playlist</p>
                     </Link>
                     <Link to=""  className="row">
                         <p className="row__rank">#3</p>
-                        {/* <figure className="row__image"></figure> */}
                         <div className="row__image-slot">
-                            <FontAwesomeIcon className="row__image" icon="gift" />
+                            <img className="row__image" src={celebration} alt="celebration" />
                         </div>
                         <p className="row__title">Gift Notification</p>
                     </Link>
